27ae78b8c569cc9b111b82dfc8b679359942cb53
shodayuki <yshoda1227@gmail.com>
2020-03-18 20:56:55 +0900

    テキスト入力をブラウザに表示する実装

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import React from 'react';
 import CommentList from './components/CommentList/CommentList';
 import Form from './components/Form/Form';
 
-function App() {
-  const comments = [
-    'テスト1',
-    'テスト2',
-    'テスト3',
-  ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="App">
-      <Form onSubmit={(body) => {
-        console.log('body? : ', body);
-      }} />
-      <CommentList comments={comments}></CommentList>
-    </div>
-  );
+    this.state = {
+      comments: ['初期コメント']
+    }
+
+    this.addComment = this.addComment.bind(this);
+  }
+
+  addComment(comment) {
+    this.setState({
+      comments: [...this.state.comments, comment]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form onSubmit={(this.addComment)}/>
+        <CommentList comments={this.state.comments} />
+      </div>
+    );
+  }
 }
 
 export default App;
